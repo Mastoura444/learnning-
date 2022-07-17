db31afcc209f9989e473fde769003a9133164178
Mastoura444 <Mastoura444@gmail.com>
2022-07-17 10:41:03 +0300

    this is first codecommit

--- ec2Instance/provider.tf
+++ ec2Instance/provider.tf
@@ -12,7 +12,7 @@ version = "4.18.0"
 }
 }
 }
-# Define AWS as a provider
+# Define AWS as a provider @@@@@@@@
 provider "aws" {
 shared_credentials_files = [ ".aws/credentials" ]
 region = var.aws_region
